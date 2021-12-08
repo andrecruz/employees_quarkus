578d87a1409a7eb2b5e3e6035ada41df1d8b8386
andrecruz <andrefilipecruz.87@gmail.com>
2021-12-08 16:09:21 +0000

    employees-basic-rest-endpoints:
        * postgreSQL database configured in application.properties;
        * data source inserted in import.sql;
        * unit tests updated;
        * javadoc updated;

--- src/main/java/com/employeeswebapi/Employee.java
+++ src/main/java/com/employeeswebapi/Employee.java
@@ -1,19 +1,31 @@
 package com.employeeswebapi;
 
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+/* Employee Entity */
+
+@Entity
 @AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
 @Getter
 @Setter
 @Builder
 public class Employee {
 
+    @Id
     @Setter(value = AccessLevel.NONE)
-    private int employeeID;
+    private Long employeeID;
     private String firstName;
     private String lastName;
     private Double baseSalary;
--- src/main/java/com/employeeswebapi/EmployeesResource.java
+++ src/main/java/com/employeeswebapi/EmployeesResource.java
@@ -1,7 +1,6 @@
 package com.employeeswebapi;
 
-import java.util.Set;
-import java.util.stream.Collectors;
+import java.util.List;
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
 import javax.ws.rs.Consumes;
@@ -18,35 +17,20 @@ import javax.ws.rs.core.MediaType;
 @Path("/employees")
 public class EmployeesResource {
 
-    private final static Employee EMPLOYEE_1 = new Employee(1, "Thomas", "Baker", 2500.00);
-    private final static Employee EMPLOYEE_2 = new Employee(2, "Andre", "Cruz", 3300.00);
-    private final static Employee EMPLOYEE_3 = new Employee(3, "John", "Campbell", 2800.00);
-    private final static Employee EMPLOYEE_4 = new Employee(4, "Andrea", "Vieira", 2800.00);
-
     @Inject
     EmployeesManager employeesManager;
-
-    public Set<Employee> employeeResource() {
-        employeesManager.addNewEmployee(EMPLOYEE_1);
-        employeesManager.addNewEmployee(EMPLOYEE_2);
-        employeesManager.addNewEmployee(EMPLOYEE_3);
-        employeesManager.addNewEmployee(EMPLOYEE_4);
-
-        return employeesManager.getEmployeeList();
-    }
 
     @GET
     @Path("/getAllEmployees")
     @Produces(MediaType.APPLICATION_JSON)
-    public Set<Employee> getAllEmployees() {
-        employeeResource();
-        return employeesManager.getEmployeeList().stream().collect(Collectors.toSet());
+    public List<Employee> getAllEmployees() {
+        return employeesManager.getAllEmployees();
     }
 
     @GET
     @Path("/getEmployeeByID/{employeeID}")
     @Produces(MediaType.APPLICATION_JSON)
-    public Employee getEmployeeByID(@PathParam("employeeID") int employeeID) {
+    public Employee getEmployeeByID(@PathParam("employeeID") Long employeeID) {
         return employeesManager.getEmployeeByID(employeeID);
     }
 
@@ -54,25 +38,23 @@ public class EmployeesResource {
     @Path("/addNewEmployee")
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
-    public Set<Employee> addNewEmployee(final Employee employee) {
-        employeesManager.addNewEmployee(employee);
-        return employeesManager.getEmployeeList();
+    public boolean addNewEmployee(final Employee employee) {
+        return employeesManager.addNewEmployee(employee);
     }
 
     @DELETE
     @Path("/deleteEmployee/{employeeID}")
     @Produces(MediaType.APPLICATION_JSON)
-    public Set<Employee> deleteEmployee(@PathParam("employeeID") final int employeeID) {
-        employeesManager.deleteAnEmployee(employeeID);
-        return employeesManager.getEmployeeList();
+    public boolean deleteEmployee(@PathParam("employeeID") final Long employeeID) throws Throwable {
+        return employeesManager.deleteAnEmployee(employeeID);
     }
 
     @PUT
     @Path("/updateEmployeeInfo/{employeeID}")
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
-    public Set<Employee> updateEmployeeInfo(@PathParam("employeeID") final int employeeID, final Employee employee) {
+    public void updateEmployeeInfo(@PathParam("employeeID") final Long employeeID, final Employee employee) throws Throwable {
         employeesManager.updateEmployeeInfo(employeeID, employee);
-        return employeesManager.getEmployeeList();
     }
+
 }
